74194845d0722f051c6d3f3f27d12759cad46e1a
fengmiaosen <fengmiaosen@gmail.com>
2026-01-06 17:43:23 +0800

    test: 添加 Vercel 部署测试文本

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      hello, next.js
+      hello, next.js - vercel deply test
     </div>
   );
 }
